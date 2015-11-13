bfc338c8a7f09d6ec69994392f99571f9ad47a52
Madhuri Palle <madhuri.palle@yahoo.com>
2015-11-12 20:17:33 -0500

    added google maps and geocoding scripts, div for map with id=‘map’ and initMap() is the js file

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,99 +1,117 @@
+function initMap() {
+  
+  var myLatLng = {lat: -25.363, lng: 131.044};
 
-	function findDistance(yours, other) {
-		var latitude = 0;
-		var longitude = 0;
-		var yourLoc = yours;
-		var otherLoc = other;
-		getLat(yourLoc, function(yourLat) {
-			getLat(otherLoc, function(otherLat) {
-				getLon(yourLoc, function(yourLon) {
-					getLon(otherLoc, function(otherLon) {
-							
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    scrollwheel: false,
+    zoom: 4
+  });
+
+  // Create a marker and set its position.
+  var marker = new google.maps.Marker({
+    map: map,
+    position: myLatLng,
+    title: 'Hello World!'
+  });
+}
+
+
+function findDistance(yours, other) {
+	var latitude = 0;
+	var longitude = 0;
+	var yourLoc = yours;
+	var otherLoc = other;
+	getLat(yourLoc, function(yourLat) {
+		getLat(otherLoc, function(otherLat) {
+			getLon(yourLoc, function(yourLon) {
+				getLon(otherLoc, function(otherLon) {
+
 							// stuff goes here
 							
 						});
-					});
-				});
 			});
-    }
-    
-    function findlonglat(address){
-    	getLat(address, function(yourLat){
-    			getLon(address, function(yourLon){
-    				document.getElementById('long').value = yourLon;
-	  				document.getElementById('lat').value = yourLat;
-	  				console.log(yourLon+" "+yourLat);
-    			});
-    	});
-    }
-    
-	
-	function getLat(address, callback) {
-        if (address) {
-            var geocoder = new google.maps.Geocoder();
-            geocoder.geocode({ 'address': address }, function (results, status) {
-                var result = 0;
-				if (status == google.maps.GeocoderStatus.OK) {
-                    if (results[0]) {
-						var lati = results[0].geometry.location.lat();
-						result = lati;
-                    }
+		});
+	});
+}
+
+function findlonglat(address){
+	getLat(address, function(yourLat){
+		getLon(address, function(yourLon){
+			document.getElementById('long').value = yourLon;
+			document.getElementById('lat').value = yourLat;
+			console.log(yourLon+" "+yourLat);
+		});
+	});
+}
+
+
+function getLat(address, callback) {
+	if (address) {
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode({ 'address': address }, function (results, status) {
+			var result = 0;
+			if (status == google.maps.GeocoderStatus.OK) {
+				if (results[0]) {
+					var lati = results[0].geometry.location.lat();
+					result = lati;
 				}
-				callback(result);
-            });
-        }
-        else {
-			callback(0);
-    	}
-    }
-    
-    function getLon(address, callback) {
-        if (address) {
-			var geocoder = new google.maps.Geocoder();
-            geocoder.geocode({ 'address': address }, function (results, status) {
-				var result = 0;
-                if (status == google.maps.GeocoderStatus.OK) {
-                    if (results[0]) {
-						var longi = results[0].geometry.location.lng();
-						result = longi;
-                    }
+			}
+			callback(result);
+		});
+	}
+	else {
+		callback(0);
+	}
+}
+
+function getLon(address, callback) {
+	if (address) {
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode({ 'address': address }, function (results, status) {
+			var result = 0;
+			if (status == google.maps.GeocoderStatus.OK) {
+				if (results[0]) {
+					var longi = results[0].geometry.location.lng();
+					result = longi;
 				}
-				callback(result);
-            });
-        	}
-        else {
+			}
+			callback(result);
+		});
+	}
+	else {
            	// error('Please enter an address');
-			callback(0);
-		}
-    }
-    function findMe() {
+           	callback(0);
+           }
+       }
+       function findMe() {
     	// alert('You are in Princeton, NJ. \nLatitude: 40.3487° N, Longitude: 74.6593° W');
     	getLocation();
     }
-	    
 
-	function getLocation()
-	{
-		if (navigator.geolocation)
-		{
-		    navigator.geolocation.getCurrentPosition(showPosition);
-		}
-		else {
+
+    function getLocation()
+    {
+    	if (navigator.geolocation)
+    	{
+    		navigator.geolocation.getCurrentPosition(showPosition);
+    	}
+    	else {
 			//alert("Sorry, geolocation is not supported by this browser.");
 		}
 	}
 	
 	function showPosition(position)
 	{
-	  	var whereAmI = "Latitude: " + position.coords.latitude;
-	  	whereAmI += ", Longitude: " + position.coords.longitude; 
-	  	
-	  	var lati = position.coords.latitude;
-	  	var longi = position.coords.longitude; 
-	  	
+		var whereAmI = "Latitude: " + position.coords.latitude;
+		whereAmI += ", Longitude: " + position.coords.longitude; 
+
+		var lati = position.coords.latitude;
+		var longi = position.coords.longitude; 
+
 	  	// stuff goes here
 	  	
 	  	console.log(whereAmI);
-	}
-	
- 
+	  }
+
