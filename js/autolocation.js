9de6644883b1c3a735d727c75452f09c94334c06
Madhuri Palle <madhuri.palle@yahoo.com>
2015-12-20 03:26:15 -0500

    markers with geolocation

--- js/autolocation.js
+++ js/autolocation.js
@@ -3,36 +3,65 @@ $("#NextStep1").click(function(){
   $('#DropMarker').modal('show');
 });
 
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 18
+function writeAddressName(latLng) {
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode({
+    "location": latLng
+  },
+  function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK)
+      console.log("yay!")
+    else
+      console.log("nooo!")
   });
-  var infoWindow = new google.maps.InfoWindow({map: map});
-
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
+function geolocationSuccess(position) {
+  var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+// Write the formatted address
+writeAddressName(userLatLng);
+
+var myOptions = {
+  zoom : 16,
+  center : userLatLng,
+  mapTypeId : google.maps.MapTypeId.ROADMAP
+};
+// Draw the map
+var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+// Place the marker
+new google.maps.Marker({
+  map: mapObject,
+  position: userLatLng
+});
+// Draw a circle around the user position to have an idea of the current localization accuracy
+var circle = new google.maps.Circle({
+  center: userLatLng,
+  radius: position.coords.accuracy,
+  map: mapObject,
+  fillColor: '#5BC0DE',
+  fillOpacity: 0.2,
+  strokeColor: '#5BC0DE',
+  strokeOpacity: 0.2
+});
+mapObject.fitBounds(circle.getBounds());
 }
+
+function geolocationError(positionError) {
+  console.log("error");
+}
+
+function geolocateUser() {
+// If the browser supports the Geolocation API
+if (navigator.geolocation)
+{
+  var positionOptions = {
+    enableHighAccuracy: true,
+timeout: 10 * 1000 // 10 seconds
+};
+navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
+}
+else
+  console.log("Your browser doesn't support the Geolocation API");
+}
+
+window.onload = geolocateUser;
