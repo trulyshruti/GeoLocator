d8a2400849c0384c47ef0a05d825f3acf26302ad
Madhuri Palle <madhuri.palle@yahoo.com>
2015-12-20 07:28:10 -0500

    rounded distance till 4 decimals, added other functionality

--- js/autolocation.js
+++ js/autolocation.js
@@ -7,6 +7,7 @@ var trueloc;
 var currentpos;
 var nextflag = 1;
 var mapObject;
+var distance;
 
 jQuery("#NextStep").click(function(){
   if(truelat==null)
@@ -27,6 +28,41 @@ jQuery("#NextStep").click(function(){
     console.log("error");
 });
 
+jQuery("#Finish").click(function(){
+calcresults();
+jQuery('#Results').modal('show');
+});
+
+
+function refreshPage() {
+  // window.location.reload(true);
+  location.reload(true);
+}
+
+function calcresults() {
+distance = getDistance(trueloc, givenloc);
+distance = Number((distance).toFixed(4));
+var resultsnode = document.getElementById("Resultstext");
+Resultstext.innerHTML = distance + " meters";
+}
+
+
+function rad(x) {
+  return x * Math.PI / 180;
+};
+
+function getDistance(p1, p2) {
+  var R = 6378137; // Earth’s mean radius in meter
+  var dLat = rad(p2.lat() - p1.lat());
+  var dLong = rad(p2.lng() - p1.lng());
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(rad(p1.lat())) * Math.cos(rad(p2.lat())) *
+    Math.sin(dLong / 2) * Math.sin(dLong / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return d; // returns the distance in meter
+};
+
 function drawline()
 {
   console.log("nextstep2");
@@ -38,10 +74,30 @@ function drawline()
     geodesic: true,
     strokeColor: '#FF0000',
     strokeOpacity: 1.0,
-    strokeWeight: 2
+    strokeWeight: 1
   });
 
   flightPath.setMap(mapObject);
+
+  hideme("NextStep");
+  unhideme("Finish");
+}
+
+
+function unhideme (divid) {
+  var item = document.getElementById(divid);
+  if (item) {
+    // console.log("unhideme: " + divid);
+    item.style.display='block';
+  }
+}
+
+function hideme (divid) {
+  var item = document.getElementById(divid);
+  if (item) {
+    // console.log("hideme: " + divid);
+    item.style.display='none';
+  }
 }
 
 
